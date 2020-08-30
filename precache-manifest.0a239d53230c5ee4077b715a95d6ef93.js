d6fcbc8b5e30d9cb64e64d8147e3f99738a17d11
fazi <fazi@fazis-MacBook-Pro.local>
2020-08-30 17:22:26 +0100

    Updates

---
+++ precache-manifest.0a239d53230c5ee4077b715a95d6ef93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d57b804549af9b239ad3e066934e4ac",
+    "url": "/matrix-solver/index.html"
+  },
+  {
+    "revision": "630865ba5ef640ed773b",
+    "url": "/matrix-solver/static/css/main.7ee73e18.chunk.css"
+  },
+  {
+    "revision": "ceab20993308939592d8",
+    "url": "/matrix-solver/static/js/2.50af8398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matrix-solver/static/js/2.50af8398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630865ba5ef640ed773b",
+    "url": "/matrix-solver/static/js/main.d92c5e27.chunk.js"
+  },
+  {
+    "revision": "064f67261f4adf5a1335",
+    "url": "/matrix-solver/static/js/runtime-main.3d15d7be.js"
+  }
+]);
